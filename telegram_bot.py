7e03f0f12d88a82a80d3064003a6cd80b6497763
galievaregina <regina-galieva-1996@mail.ru>
2022-12-01 17:49:12 +0300

    Resolve https://trello.com/c/5VwStE2a

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,22 @@ bot = telebot.TeleBot('5798771779:AAHSFF0CQgEHFm4rK_ss1-SmdrkjQzLni6M')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, text='Привет! Выбери знак зодиака')
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)  # наша клавиатура
+    key_aries = telebot.types.KeyboardButton(text='Овен')
+    key_taurus = telebot.types.KeyboardButton(text='Телец')
+    key_gemini = telebot.types.KeyboardButton(text='Близнецы')
+    key_cancer = telebot.types.KeyboardButton(text='Рак')
+    key_leo = telebot.types.KeyboardButton(text='Лев')
+    key_virgo = telebot.types.KeyboardButton(text='Дева')
+    key_libra = telebot.types.KeyboardButton(text='Весы')
+    key_scorpio = telebot.types.KeyboardButton(text='Скорпион')
+    key_sagittariu = telebot.types.KeyboardButton(text='Стрелец')
+    key_capricorn = telebot.types.KeyboardButton(text='Козерог')
+    key_aquarius = telebot.types.KeyboardButton(text='Водолей')
+    key_pisces = telebot.types.KeyboardButton(text='Рыбы')
+    keyboard.add(key_taurus, key_aries, key_gemini, key_cancer, key_leo, key_virgo, key_libra, key_scorpio,
+                 key_sagittariu, key_capricorn, key_aquarius, key_pisces)
+    bot.send_message(message.chat.id, text='Привет! Выбери знак зодиака', reply_markup=keyboard)
 
 
 bot.polling(none_stop=True)
